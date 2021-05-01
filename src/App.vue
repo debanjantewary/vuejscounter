6b4c5ab2b267fab248e12013da6812af48479f77
debanjantewary <rijutewary2@gmail.com>
2021-05-01 16:30:02 +0530

    counter

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+    <div>
+        <HelloWorld msg="Counter" />
+
+        <div class="owner">
+            <h2 class="creator">Created By:</h2>
+            <p class="name"><code>.Debanjan_Tewary()</code></p>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import HelloWorld from "./components/HelloWorld.vue";
+
+// This starter template is using Vue 3 experimental <script setup> SFCs
+// Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
+</script>
+
+<style>
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    /* margin-top: 10px; */
+    box-sizing: border-box;
+    padding: none;
+    margin: 0;
+    justify-content: center;
+    align-items: center;
+}
+
+.logoimg {
+    max-height: 30vh;
+}
+
+.owner {
+    min-width: 100vw;
+    max-height: 10vh;
+    margin: none;
+}
+.owner > h2 {
+    color: green;
+    font-size: 20px;
+    padding-bottom: 0;
+    margin-bottom: 0;
+}
+
+.owner > p {
+    font-size: 20px;
+    font-weight: bold;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,117 @@
+<template>
+<div>
+
+ <h1>{{ msg }}</h1>
+<div class="intro">
+<span class="icon"></span>
+  <p>
+    <a href="https://debanjantewary.github.io/fetchapi" target="_blank">
+   Github Api fetch (react)
+    </a>
+    
+  </p>
+</div>
+<div class="maincontent">
+
+<div class="buttons">
+
+  <button class="dbtn" @click="state.count--">Decrease</button>
+  <button class="countbtn"> {{ state.count }}</button>
+
+  <button class="dbtn" @click="state.count++">increase</button>
+
+</div>
+
+  <div class="counting">
+
+  <span v-if="state.count===0">nutral</span>
+  <span v-else>
+    
+    <span>
+<span v-if="state.count>=10">
++ 10 reached
+  </span>
+      <span v-if="state.count>0 & state.count<10">
+            Positive (under 10)
+  </span>
+  <span v-else-if="state.count<=-10">
+  -10 reached  
+  </span>
+    <span v-else-if="state.count<0 & state.count>-10"> NEGETIVE
+  </span>
+
+    </span>
+    
+    </span>
+  </div>
+</div>
+
+</div>
+</template>
+<script setup>
+  import { defineProps , reactive} from 'vue'
+
+defineProps({
+  msg: String
+})
+
+const state = reactive({ count: 0 })
+
+
+</script>
+
+<style scoped>
+a {
+  color: #42b983;
+  text-decoration: none;
+}
+
+.dbtn{
+border: none;
+background-color: green;
+color: whitesmoke;
+border-radius: 5px;
+padding: 10px 10px;
+font-size: 20px;
+margin: 5px 5px;
+cursor: pointer;
+  }
+  .counting{
+
+    margin: 10px;
+  }
+
+  .countbtn{
+
+ border: none;
+background-color: rgb(5, 4, 14);
+color: whitesmoke;
+border-radius: 5px;
+padding: 10px 10px;  
+font-size: 30px; 
+  }
+
+  span {
+font-size: 30px;
+    }
+
+.intro{
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+    .icon{
+
+        background: url("https://oflisback.github.io/react-favicon/img/github.ico");
+        height: 30px;
+        width: 32px;
+       
+        display: inline;
+
+        
+    }
+</style>
+
+
